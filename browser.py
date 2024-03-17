dcedd88434be3f5b643d2409f50dde9682c52e1d
the80hz <dnl.vlgv@mail.ru>
2024-03-17 16:40:38 +0400

    перешел на playwright

--- browser.py
+++ browser.py
@@ -6,36 +6,27 @@ from utils import make_request, clean_filename, extract_domain, rm_prefix, HEADE
 
 
 async def handle_page(page):
-    # Функция для обработки содержимого страницы
-    # Например, скрыть определенные блоки
-    titles = await page.query_selector_all('.title_link')
-    print(titles)
-    for title in titles:
-        href = await title.get_attribute('href')
-        full_url = href
-        # Если URL удовлетворяет условию (пример с заглушкой функции)
-        if await is_manga_downloaded(full_url):  # Функция для проверки URL
-            await title.evaluate("element => element.style.display='none'")
-
-
-async def monitor_new_pages(browser):
-    # Функция для отслеживания создания новых страниц и вкладок
-    async def handle_new_page(page):
-        # Добавляем обработчик для каждой новой страницы
-        await handle_page(page)
-    browser.on("page", handle_new_page)
+    # Ваша логика обработки страницы
+    print("Обрабатываем")
 
 
 async def main():
     async with async_playwright() as p:
-        browser = await p.chromium.launch(headless=False)  # Запуск браузера в видимом режиме
+        browser = await p.chromium.launch(headless=False)
         page = await browser.new_page()
-        await monitor_new_pages(browser)  # Начать отслеживание новых страниц
-        await page.goto('https://example.com')  # Перейти на начальную страницу
-        await handle_page(page)  # Обработать начальную страницу
 
-        # Ожидаем некоторое время или пользовательского ввода, чтобы скрипт не завершился сразу
-        await asyncio.sleep(300)  # Ожидание 5 минут
+        # Подписываемся на событие навигации
+        page.on('load', lambda frame: asyncio.create_task(handle_page(page)))
+        #page.on('load', lambda: asyncio.create_task(handle_page(page)))
+
+        await page.goto('https://google.com')
+        # Пример перехода на новую страницу через клик или другое действие
+        # Предположим, что есть ссылка с идентификатором link-to-click
+        # await page.click('#link-to-click')
+
+        # Чтобы скрипт не завершился сразу, ждем некоторое время или пользовательского ввода
+        await asyncio.sleep(300)  # Даём 5 минут на интерактивное использование браузера
+
         await browser.close()
 
 
